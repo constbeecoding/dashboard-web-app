c4471117a925f148cac76609debcd8e8325fba34
Bee Kay <brittkcrouch@gmail.com>
2021-02-26 17:38:44 -0600

    Updated Javascript, beginning scripting for Alert

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // JS Functionality
 // 
+
+//ALERT BANNER
+
+const alertBanner = document.getElementById("alert");
+alertBanner.innerHTML = `
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>7</strong> overdue tasks. </p>
+<p class="alert-banner-close"> x </p>
+</div>
+`
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if(element.classList.contains("alert-banner-close")) {
+        alert.style.display ="none"
+    }
+
+});
+
+console.log(alert);
