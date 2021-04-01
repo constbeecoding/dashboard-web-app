9647acb9dd360f3d76aa752d2f09184c20cbc628
Bee Kay <brittkcrouch@gmail.com>
2021-03-31 21:25:27 -0500

    Added Notification JS to the Moon, Work In Progress

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,26 @@
 // JavaScript Functionality
 
-//ALERT BANNER
+/////////////////////
+//NOTIFICATION MOON//
+/////////////////////
+
+const notificationMoon = document.getElementById("notifications");
+notificationMoon.innerHTML = `
+<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>7</strong> overdue tasks. </p>
+<p class="notification-close"> x </p>
+</div>
+`
+notificationMoon.addEventListener('click', e => {
+    const element = e.target;
+    if(element.classList.contains("notification-close")) {
+        notificationMoon.style.display ="none"
+    }
+
+});
+/////////////////////
+////ALERT BANNER/////
+/////////////////////
 
 const alertBanner = document.getElementById("alert");
 alertBanner.innerHTML = `
@@ -113,7 +133,7 @@ let trafficDataDay= {
 let trafficOptions = {
     aspectRatio: 2.5,
     animation: { 
-        duration: 0
+        duration: 1000,
     },
     scales: {
         yAxes: [{
@@ -259,6 +279,26 @@ send.addEventListener('click', () => {
     }
     });
 
+
+function filter() {
+    
+    var input, ul, li, a, i;
+        input = document.getElementById("userID");
+        filterValue = input.value.toUpperCase();
+        ul = document.getElementById("gallery");
+        li = ul.getElementsByTagName("li");
+        
+        for (i = 0 ; i < li.length ; i++){
+            a = li[i].getElementsByTagName("a")[0];
+
+            if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+                li[i].style.display = "";
+                
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
 //LOCAL STORAGE
 
 //If save is clicked, save settings to local storage.
